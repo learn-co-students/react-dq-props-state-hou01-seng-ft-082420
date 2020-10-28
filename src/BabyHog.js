ddb064333093be270d8e46fcb452eec4d4efadb8
stephenandersondev <stephenandersondev@gmail.com>
2020-10-28 09:46:38 -0500

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -12,6 +12,10 @@ export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      weight: 50
+    }
   }
 
   changeWeight = (e) => {
@@ -22,25 +26,41 @@ export default class BabyHog extends Component {
     })
   }
 
+  eyeColorMapper = (color) => {
+    let image = null
+    switch (color) {
+      case 'blue':
+        image = BlueBaby
+        break;
+      case 'sun':
+        image = SunBaby
+        break;
+      case 'glowing':
+        image = GlowingBaby
+        break;
+    }
+    return image
+  }
+
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
-        <Button name="+">
+        <h1>Name: {this.props.offSpring.name} </h1>
+        <h3>Weight: {this.state.weight} </h3>
+        <h3>Hobby: {this.props.offSpring.hobby} </h3>
+        <h4>Eye Color: {this.props.eyeColor}</h4>
+
+        <Button name="+" onClick={this.changeWeight} >
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button name="-" onClick={this.changeWeight} >
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={this.eyeColorMapper(this.props.eyeColor)} style={{ height: '200px' }} alt="MasterBlasterJrJr" />
         </div>
-        
+
       </li>
     )
   }
